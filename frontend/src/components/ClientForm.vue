bc247c838cf49435334b8de10c57bf09c2916091
Nikita <ngolev.bus@gmail.com>
2023-04-24 19:23:21 +1000

    Start work on the form prototype

---
+++ frontend/src/components/ClientForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="px-4 py-3 my-5 text-center">
+        <h1 class="display-5 fw-bold">Client Application</h1>
+        <p class="mb-4">Please enter your portfolio details below.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ClientForm",
+};
+</script>
+
+<style lang="scss"></style>
